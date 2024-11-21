4415b1870f8f250444303c78b318a579c43fcf33
AlaaZreekie <alaasama282@gmail.com>
2024-11-21 12:59:04 +0300

    end

--- JDB/src/Main.java
+++ JDB/src/Main.java
@@ -7,13 +7,13 @@ public class Main {
     public static void main(String[] args) {
         DbFunctions db=new DbFunctions();
         Connection conn=db.connect_to_db("TransactionDbForJava","postgres","hiast");
-        //db.createTable(conn,"employee");
-        //db.insert_row(conn,"employee","Rajat","India");
-        //db.update_name(conn,"employee","Rahul","Raj");
-        //db.search_by_name(conn,"employee","abhishek");
-        //db.delete_row_by_name(conn,"employee","abhishek");
-        //db.delete_row_by_id(conn,"employee",4);
-        //db.read_data(conn,"employee");
+        db.createTable(conn,"employee");
+        db.insert_row(conn,"employee","Alaa","Syria");
+        db.update_name(conn,"employee","Alaa","ALaaa");
+        db.search_by_name(conn,"employee","Zrekie");
+        db.delete_row_by_name(conn,"employee","Zrekie");
+        db.delete_row_by_id(conn,"employee",4);
+        db.read_data(conn,"employee");
         // db.delete_table(conn,"employee");
     }
 
